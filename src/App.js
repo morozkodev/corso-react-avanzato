5d8f0aa03a18a1760bd7bb0f6bf55cc355ad0db8
Morozko <fugerit.develop@gmail.com>
2021-01-18 17:32:38 +0100

    Esempio antd
    https://ant.design/components/list/#API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import { List, Avatar, Alert, Pagination } from 'antd';
 
-class App extends React.Component {
+import useFetch from './hooks/useFetch';
+import { getUsers } from './hooks/userProvider';
+
+const UserList = () => {
+
+    const [{isLoading, hasError, response}, setParams] = useFetch( {
+		providerFunc: getUsers,
+		initialParams: { per_page: 4 }
+	} );
 	
-	constructor(props) {
-		super( props );
-	}
+	/*
+		https://ant.design/components/list/#API
+	 */
 	
-	render() {
-		return (
-			<div className="App">
-				<h1>Pagina iniziale</h1>
-			</div>
-		);
-	}
-	 
+    function cambiaPagina( current, pageSize ){
+        setParams( { page:current, per_page: pageSize } );
+    }
+	
+    return (
+        <React.Fragment>
+            <h1>User List</h1>
+			<List
+				header={"Lista utenti"}
+				footer={response && <Pagination defaultCurrent={response.page} total={response.total} pageSize={response.per_page} onChange={cambiaPagina} />}
+				loading={isLoading}
+				bordered
+				dataSource={response && response.data}
+				pagination={true}
+				renderItem={
+					user => <List.Item key={user.id}>
+						<List.Item.Meta avatar={<Avatar src={user.avatar}/>}
+							title={`${user.first_name} ${user.last_name}`}
+							description={user.email}></List.Item.Meta>
+					</List.Item>
+				}>
+			</List>
+			{!isLoading && hasError && (
+				<Alert message="Error" type="error" showIcon />
+			)} 
+        </React.Fragment>
+    )
+}
+
+function App (){
+    return (
+        <div className="App">
+            <h1>App React</h1>
+            <UserList />
+        </div>
+    )
 }
 
 export default App;
