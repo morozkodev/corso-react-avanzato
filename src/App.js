e2f8a2664f1c4bb98c50392f8d94025e12c9707b
morozko <fugerit.develop@gmail.com>
2021-01-19 12:41:26 +0100

    Esempio context multiple

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+const ThemeContext = React.createContext( 'light' );
+const UserContext = React.createContext( { name: 'Guest' } );
+
+function Content() {
+	return(
+		<ThemeContext.Consumer>
+			{theme => (
+				<UserContext.Consumer>
+						{user => (
+							<ProfilePage user={user} theme={theme}/>
+						)}
+				</UserContext.Consumer>
+			)}
+		</ThemeContext.Consumer>
+	)
+}
+
+function ProfilePage(props){
+    return(
+        <div>
+            <p>{props.user}</p>
+            <p>{props.theme}</p>
+        </div>
+    );
+}
+
+function Layout() {
+	return (
+		<div>
+			<Content />
+		</div>
+	);
+}
 
 class App extends React.Component {
 	
-	constructor(props) {
-		super( props );
-	}
-	
 	render() {
+		const user = { name: 'Lorenzo' };
+		const theme = 'dark';
 		return (
 			<div className="App">
-				<h1>Pagina iniziale</h1>
+				<h1>App React Context</h1>
+				<ThemeContext.Provider value={theme}>
+					<UserContext.Provider value={user}>
+						<Layout/>
+					</UserContext.Provider>
+				</ThemeContext.Provider>
 			</div>
 		);
 	}
