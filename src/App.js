482d404126b53706ddb53c9843422399365a26fa
morozko <fugerit.develop@gmail.com>
2021-01-19 10:48:14 +0100

    Esempio provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // esempio di uso di context
 import React from 'react';
 import './App.css';
-const ThemeContext = React.createContext( { label2: 'Bottone Label 2' });
-const ThemeContext3 = React.createContext('Bottone context 3');
+const ThemeContext = React.createContext( { label1: 'Bottone Label 1' } );
 
 function App (){
     return (
         <div className="App">
             <h1>Esempio react context</h1>
-            <Toolbar valore="Test"></Toolbar>
+			<ThemeContext.Provider value={{ label1: 'Bottone Provider 1' }}>
+				<Toolbar valore="Test"></Toolbar>
+			</ThemeContext.Provider>
         </div>
     )
 }
@@ -16,30 +17,15 @@ function App (){
 function Toolbar(props){
     return (
         <div>
-            <ThemedButton1 valore={props.valore}></ThemedButton1>
-			<ThemedButton2></ThemedButton2>
-			<ThemedButton3></ThemedButton3>
+            <ThemedButton1></ThemedButton1>
         </div>
     )
 }
 
 class ThemedButton1 extends React.Component{
+	static contextType = ThemeContext
 	render() {
-		return <button>{this.props.valore}</button>
-	}
-}
-
-class ThemedButton2 extends React.Component{
-	static contextType = ThemeContext;
-	render() {
-		return <button>{this.context.label2}</button>
-	}
-}
-
-class ThemedButton3 extends React.Component{
-	static contextType = ThemeContext3;
-	render() {
-		return <button>{this.context}</button>
+		return <button>{this.context.label1}</button>
 	}
 }
 
