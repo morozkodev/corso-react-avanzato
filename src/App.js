fd64cf347250969e6937053624a1ee24efedc507
morozko <fugerit.develop@gmail.com>
2021-01-20 17:00:39 +0100

    Esempio lazy load

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+const Test1 = React.lazy( () => import( './lazy/Test1' ) );
+const Test2 = React.lazy( () => import( './lazy/Test2' ) );
+const Test3 = React.lazy( () => import( './lazy/Test3' ) );
+
 
 class App extends React.Component {
 	
@@ -10,8 +16,37 @@ class App extends React.Component {
 	
 	render() {
 		return (
-			<div className="App">
-				<h1>Pagina iniziale</h1>
+			<div>
+				<Router>
+					<div className="container">
+                       <nav>
+                            <ul>
+								<li><Link to="/test1">Test 1</Link></li>
+                                <li><Link to="/test2">Test 2</Link></li>
+                                <li><Link to="/test3">Test 3</Link></li>
+                            </ul>
+                        </nav>
+						<Suspense fallback={<div>Loading</div>}>
+							<Switch>								
+								<Route exact path="/test2">
+									<section>
+										<Test2/>
+									</section>
+								</Route>
+								<Route exact path="/test3">
+									<section>
+										<Test3/>
+									</section>
+								</Route>
+								<Route path="/">
+									<section>
+										<Test1/>
+									</section>
+								</Route>
+							</Switch>
+						</Suspense>
+					</div>
+				</Router>
 			</div>
 		);
 	}
---
+++ src/lazy/Test3.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class Test1 extends React.Component{
+    render(){
+        return(
+            <section>
+                <div><h1>Test 3</h1></div>
+            </section>
+        )
+    }
+}
