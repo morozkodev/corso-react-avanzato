ac9ec830155770f890215a8a1f0b6c196974ad26
morozko <fugerit.develop@gmail.com>
2021-01-20 16:21:40 +0100

    Esempio HOC

--- src/App.js
+++ src/App.js
@@ -2,6 +2,37 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 
+class MyList extends React.Component {
+	
+	render() {
+		 return(
+			<div>
+				{this.props.data.map(
+					( text, index ) => (
+						<p key={index}>{text}</p>
+				))}
+			</div>
+		)
+	}
+
+}
+
+const withData = ( Comp, data ) => {
+	class WithData extends React.Component {
+		render() {
+			return <Comp data={data}/>
+		}
+	}
+	WithData.displayName = `WithData(${getDisplayName(Comp)})`;
+	return WithData;
+}
+
+const getDisplayName = WrappedComp => {
+	return WrappedComp.displayName || WrappedComp.Name || "Component";
+}
+
+const MyListData = withData( MyList, ['java', 'js', 'php', 'c++'] );
+
 class App extends React.Component {
 	
 	constructor(props) {
@@ -11,7 +42,8 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Pagina iniziale</h1>
+				<h1>React HOC</h1>
+				<MyListData/>
 			</div>
 		);
 	}
